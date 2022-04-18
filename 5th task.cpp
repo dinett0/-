e6f8050e2694778684900867c3a7722f105a33a8
dinett0 <100412070+dinett0@users.noreply.github.com>
2022-04-19 00:37:48 +0300

    Add files via upload

---
+++ 5th task.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+const int SIZE = 100;
+
+void Search(int A[], int SIZE, int search_value) {
+	int begin, end, mid;
+	begin = 0;
+	end = SIZE;
+	int i = 0;
+
+	while (true)
+	{
+		mid = (end + begin) / 2;
+		if (A[mid] > search_value) {
+			end = mid;
+			cout << "Iteration " << i++ << ", not found\n";
+		}
+		else if (A[mid] < search_value) {
+			begin = mid;
+			cout << "Iteration " << i++ << ", not found\n";
+		}
+		else {
+			cout << "Iteration " << i << ". Found on the position " << mid;
+			break;
+		}
+		if (i > 10) {
+			break;
+		}
+	}
+
+	cout << endl;
+}
+
+void show(int arr[], int size) {
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+
+	int A[SIZE];
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		A[i] = i+1;
+	}
+
+	show(A, SIZE);
+
+	int search_value;
+	cout << "Enter value: ";
+	cin >> search_value;
+	Search(A, SIZE, search_value);
+
+	/// ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	int C[1000];
+	for (size_t i = 0; i < 1000; i++)
+	{
+		C[i] = i + 1;
+	}
+
+	cout << "Enter value: ";
+	cin >> search_value;
+
+	Search(A, 1000, search_value);
+}
