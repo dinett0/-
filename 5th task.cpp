79d285e06172a72cdad3101fa08c4c50be9f8166
dinett0 <100412070+dinett0@users.noreply.github.com>
2022-04-22 22:14:25 +0300

    Update 5th task.cpp

--- 5th task.cpp
+++ 5th task.cpp
@@ -1,71 +1,32 @@
 #include <iostream>
+#include <string>
+#include <cctype>
 using namespace std;
-
-const int SIZE = 100;
-
-void Search(int A[], int SIZE, int search_value) {
-	int begin, end, mid;
-	begin = 0;
-	end = SIZE;
-	int i = 0;
-
-	while (true)
-	{
-		mid = (end + begin) / 2;
-		if (A[mid] > search_value) {
-			end = mid;
-			cout << "Iteration " << i++ << ", not found\n";
-		}
-		else if (A[mid] < search_value) {
-			begin = mid;
-			cout << "Iteration " << i++ << ", not found\n";
-		}
-		else {
-			cout << "Iteration " << i << ". Found on the position " << mid;
-			break;
-		}
-		if (i > 10) {
-			break;
-		}
-	}
-
-	cout << endl;
-}
-
-void show(int arr[], int size) {
-	for (size_t i = 0; i < SIZE; i++)
-	{
-		cout << arr[i] << " ";
-	}
-	cout << endl;
-}
 
 int main() {
 
-	int A[SIZE];
+	string test = "In computer programming, a null-terminated string is a character string stored as an array containing the characters and terminated with a null character. Alternative names are C string, which refers to the C programming language and ASCIIZ. ", exp = "string";
+	int i = 0, eh;
+	bool checker = true;
 
-	for (size_t i = 0; i < SIZE; i++)
+	while (test[i]!='\0')
 	{
-		A[i] = i+1;
+		checker = true;
+		if (test[i] == exp[0]) {
+			eh = i;
+			for (int j = 0; j < exp.length(); j++)
+			{
+				if (exp[j] != test[eh]) {
+					checker = false;
+					break;
+				}
+				eh++;
+			}
+			if (checker) {
+				cout << "Index " << i;
+			}
+		}
+		i++;
 	}
 
-	show(A, SIZE);
-
-	int search_value;
-	cout << "Enter value: ";
-	cin >> search_value;
-	Search(A, SIZE, search_value);
-
-	/// ///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	int C[1000];
-	for (size_t i = 0; i < 1000; i++)
-	{
-		C[i] = i + 1;
-	}
-
-	cout << "Enter value: ";
-	cin >> search_value;
-
-	Search(A, 1000, search_value);
 }
